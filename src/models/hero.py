4be42f0f1e2b8b1552639b9ce344096d479a40ce
Sleptsova Zlata <zvsleptsova@edu.hse.ru>
2026-05-16 17:12:06 +0300

    fix hero skill lookup

--- src/models/hero.py
+++ src/models/hero.py
@@ -16,9 +16,12 @@ class Hero:
     skill_2_level: int
 
     def get_skill(self, skill_name: str) -> int:
-       
-        if self.skill_1_name == skill_name:
+        skill_name = skill_name.strip().lower()
+
+        if self.skill_1_name.strip().lower() == skill_name:
             return self.skill_1_level
-        if self.skill_2_name == skill_name:
+
+        if self.skill_2_name.strip().lower() == skill_name:
             return self.skill_2_level
+
         return 0
